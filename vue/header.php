b474f9605c709f3edea9c40e385bd3b06ddf2042
M-Habib-Boujenfa <habib-boujenfa@hotmail.fr>
2023-04-18 22:36:55 +0200

    fin projet recettes

--- vue/header.php
+++ vue/header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $titre ?? "Liste de recettes" ?></title>
+    <link rel="stylesheet" href="style/navbar.css">
 </head>
 
 <body>
